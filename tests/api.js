61f10bc3be7b307cf87d748961a41b835c190b50
Leo Zhang <leo@lehaozhang.com>
2013-12-30 02:56:11 -0800

    Started work on API. Doesn't work yet, not sure why. Suspect bad MD5 hash.

---
+++ tests/api.js
@@ -0,0 +1,9 @@
+var api = require('../lib/api');
+
+api.cleverbot.think('Hi');
+
+// var http = require('http');
+// var req = http.get('http://www.cleverbot.com/webservicemin?start=y', function(res){
+//   console.log('r', res);
+// });
+// req.end();
