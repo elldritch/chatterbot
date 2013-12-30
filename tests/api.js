c7c4cbe7a41d738b229b05217a811b250f5f75d7
Leo Zhang <leo@lehaozhang.com>
2013-12-30 15:26:55 -0800

    API works now -- was POST, not GET.

--- tests/api.js
+++ tests/api.js
@@ -1,9 +1,20 @@
 var api = require('../lib/api');
 
-api.cleverbot.think('Hi');
+var bot = api.cleverbot();
+bot.think('Hi', function(err, res){
+  if(err){
+    console.error('error', err);
+  }
+  console.log(res);
+  bot.think('What about you?', function(err, res){
+    console.log(res);
+  });
+});
 
-// var http = require('http');
-// var req = http.get('http://www.cleverbot.com/webservicemin?start=y', function(res){
-//   console.log('r', res);
+// var request = require('request');
+// request('http://www.cleverbot.com/webservicemin?start=y', function(err, res, body){
+//   console.log('r', body);
 // });
-// req.end();
+// request('http://www.cleverbot.com/webservicemin?sub=Say&stimulus=Hello%21+Have+you+a+question+for+me%3F&islearning=1&start=y&icognoid=wsf&cleanslate=false&fno=0&icognocheck=81d7212476665bac07d6e0f357d2df44', function(err, res, body){
+//   console.log('r2', body);
+// });
