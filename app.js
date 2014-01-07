dc9aa24d1035d3578d784989a3ec39aac5d8f253
Leo Zhang <leo@lehaozhang.com>
2014-01-07 14:28:56 -0800

    Cleanup of Bower components.

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
-var socketio = require('socket.io');
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path')
+  , socketio = require('socket.io');
 
 var app = express();
 
-// all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -26,13 +19,11 @@ app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'components')));
 
-// development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('*', routes.index);
 
 var server = http.createServer(app);
 var io = socketio.listen(server);
