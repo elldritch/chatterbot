800be72ce9edbf83452559ae9a7ed3b7797bd510
Larry Xu <larry@larryxu.com>
2014-01-06 16:41:52 -0800

    Updated chat UI

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,20 +1,27 @@
 var result = '';
+var recognizing = false;
 if (!('webkitSpeechRecognition' in window)) {
   console.log('webkitSpeechRecognition not available, use fallback');
   // insert firefox/opera fallback solution here
 } else {
   var recognition = new webkitSpeechRecognition();
   recognition.lang = 'en-US'; // English US, can be modified
+  recognition.continuous = true;
 
   recognition.onstart = function() {
     recognizing = true;
+    toggleIcon('ion-ios7-mic', 'ion-ios7-mic-off', 'Stop Recording');
   }
 
   recognition.onresult = function(event) {
     console.log(event);
 
-    result = capitalize(event.results[0][0].transcript);
+    var index = event.results.length - 1;
+    result = capitalize(event.results[index][0].transcript.trim());
     $('#chatarea').append(domWrap(result));
+    var offset = $('#chatarea').height() - $('#chatbox').height();
+    if(offset > 0)
+      $('#chatbox').scrollTop(offset);
   }
 
   recognition.onerror = function(event) {
@@ -24,10 +31,15 @@ if (!('webkitSpeechRecognition' in window)) {
   recognition.onend = function() {
     recognizing = false;
     result = '';
+    toggleIcon('ion-ios7-mic-off', 'ion-ios7-mic', 'Record');
   }
 }
 
-function record() {
+function toggleVoice() {
+  if(recognizing) {
+    recognition.stop();
+    return;
+  }
   recognition.start();
 }
 
@@ -37,4 +49,10 @@ function domWrap(s) {
 
 function capitalize(s) {
   return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+function toggleIcon(remove, add, title) {
+  $('#record').removeClass(remove);
+  $('#record').addClass(add);
+  $('#record').attr('title', title);
 }
